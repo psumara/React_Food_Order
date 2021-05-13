ddf9a5c5895051ad0ded65485e94fa15c3653de8
p.sumara <sumara0498@icloud.com>
2021-05-13 13:31:04 +0200

    Create project structure, header with sample image, Card component which holds all Meals components and MealItemForm which include form to add item to the cart. This commit also includes all components styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
